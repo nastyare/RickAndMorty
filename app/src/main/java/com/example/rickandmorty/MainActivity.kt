4b3a2417384e2b43acfe205f43f9ea207ec95807
Anastasia <anastasiya.reksius@mail.ru>
2024-03-15 23:13:09 +0700

    a lil bit more

--- app/src/main/java/com/example/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -4,7 +4,6 @@ import Adapter.CharacterAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.rickandmorty.databinding.ActivityMainBinding
 import viewModel.CharactersViewModel
 import androidx.activity.viewModels
--- app/src/main/java/viewModel/characterView.kt
+++ app/src/main/java/viewModel/characterView.kt
@@ -5,7 +5,6 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.rickandmorty.RickAndMortyApiClient
 import kotlinx.coroutines.launch
